8e6d144958c7064ef2d5b02a805ce677ce200ece
JESWIN100 <josepjjeswin20@gmail.com>
2024-11-11 07:18:48 +0530

    secure change

--- socket/socket.js
+++ socket/socket.js
@@ -9,7 +9,7 @@ const app=express()
 const server=http.createServer(app)
 const io= new Server(server,{
     cors:{
-        origin:"https://chatapp-tau-five.vercel.app",
+        origin:["https://chatapp-tau-five.vercel.app"],
         methods:['GET','POST']
     }
 });
--- utils/jsonWebToken.js
+++ utils/jsonWebToken.js
@@ -8,7 +8,7 @@ import jwt from 'jsonwebtoken'
         maxAge:30 *24 *60 *60 *1000,
         httpOnly:true,
         sameSite:"strict",
-        secure:process.env.SECURE !=="development"
+        secure:true
 
     })
 
